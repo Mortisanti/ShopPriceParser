714525e206dd0ad341075be19fece94491820dd8
Mortisanti <moltigamer@gmail.com>
2021-02-23 03:11:14 -0500

    Added some gitignores

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ import re
 import json
 import csv
 
-# Create regular expression patterns
+# Regular expression patterns
 regex_shop_id = re.compile('(\d+):')
 regex_items = re.compile('(\d+),(\d+),(\d+),(\d+);')
 
-# Open prices data file and split lines based on linebreaks, combine into list as elements
+# Open prices data file and split lines based on linebreaks, store in list
 with open('prices.dat', 'r') as f:
     text = f.read()
-inventories = text.splitlines()
+shops = text.splitlines()
 
 shops_dict = {}
 item_dict = {}
-for i in range(len(inventories)):
-    shop_id = re.search(regex_shop_id, inventories[i]).group(1)
-    shop_inventory = re.findall(regex_items, inventories[i])
+# Comments go here
+for i in range(len(shops)):
+    shop_id = re.search(regex_shop_id, shops[i]).group(1)
+    shop_inventory = re.findall(regex_items, shops[i])
     item_list = []
+    # More comments here, maybe
     for item in shop_inventory:
         item_id = item[0]
         item_attributes_dict = {
@@ -38,5 +40,8 @@ for i in range(len(inventories)):
 with open('shops_dict.txt', 'w') as f:
     f.write(str(shops_dict))
 
-with open('prices.json', 'w') as f:
-    f.write(json.dumps(shops_dict))
+# with open('prices.json', 'w') as f:
+#     f.write(json.dumps(shops_dict))
+# 
+# with open('prices_preformatted.json', 'w') as f:
+#     f.write(json.dumps(shops_dict, indent=4))
